d1e6978efe799846047e1bd22b1944a065ca86c8
Jhon Moreno <jhonmorenoms@Jhons-MS-MacBook-Pro.local>
2022-02-21 12:45:13 -0500

    Prepare branch for Workshop

--- src/containers/QualifyPokemon.tsx
+++ src/containers/QualifyPokemon.tsx
@@ -1,6 +1,5 @@
 import Container from "@mui/material/Container";
 
-import { PokemonBanner, PokemonControlCard } from "src/components";
 import { useRetrieveNextPokemon, useVoteControl } from "src/utils";
 
 export const QualifyPokemon: React.FC = () => {
@@ -10,19 +9,7 @@ export const QualifyPokemon: React.FC = () => {
 
   return (
     <Container maxWidth="xs">
-      {data.pokemon && (
-        <PokemonBanner pokemon={data.pokemon} index={data.index} />
-      )}
-      {data.pokemon && (
-        <PokemonControlCard
-          onNext={data.next}
-          onPrevious={data.previous}
-          pokemon={data.pokemon}
-          onLike={onLike}
-          onDislike={onDislike}
-          voteStatus={voteStatus}
-        />
-      )}
+      
       Vote status: {voteStatus}
       <small>
         <pre>{JSON.stringify(data, null, 2)}</pre>
